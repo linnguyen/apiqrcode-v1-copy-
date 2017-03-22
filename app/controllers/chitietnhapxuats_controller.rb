4e4b8594879974be146952add156698cd6e17832
linnguyen <nguyenvanlin01041994@gmail.com>
2017-03-22 10:20:50 +0700

    hello heroku

--- app/controllers/chitietnhapxuats_controller.rb
+++ app/controllers/chitietnhapxuats_controller.rb
@@ -2,4 +2,8 @@ class ChitietnhapxuatsController < ApplicationController
 	def index
 		render json: ChiTietNhapXuat.all
 	end
+
+	def show_info
+		render json: ChiTietNhapXuat.find_by(macode: params[:macode])
+	end
 end
