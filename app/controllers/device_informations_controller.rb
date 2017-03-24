bc29b389d84439a8a57fe240031b87f587f4377a
linnguyen <nguyenvanlin01041994@gmail.com>
2017-03-24 10:15:10 +0700

    edit show_info method

--- app/controllers/device_informations_controller.rb
+++ app/controllers/device_informations_controller.rb
@@ -2,7 +2,8 @@ class DeviceInformationsController < ApplicationController
 	def show_info
          @export_bill_detail =  ExportBillDetail.find_by(qrcode: params[:qrcode])
          @export_bill =  @export_bill_detail.export_bill
-         device = @export_bill.input_output_details.where(qrcode: params[:qrcode][0..11])
+         # device = @export_bill.input_output_details.where(qrcode: params[:qrcode][0..11]) -> array of device
+         device = @export_bill.input_output_details.find_by(qrcode: params[:qrcode][0..11]) # -> one device
          staff = @export_bill.staff
          render json: {:staff => staff, :device => device}
     end
