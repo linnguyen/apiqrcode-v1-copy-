0926de2efb87d7305ec921a65c9e899580c3b6d5
linnguyen <nguyenvanlin01041994@gmail.com>
2017-04-02 23:07:00 +0700

    edit inventory method

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,15 +1,16 @@
 class InventoriesController < ApplicationController
 	def create
-		
-		inventory = Inventory.create(inventory_params)
-		if inventory.save
-			render json: inventory, status: :created
-		else
-			render_error(inventory, :unprocessable_entity)
-		end
-	end
-	private 
-	def inventory_params
-        params.require(:inventory).permit(:date_of_inventory)
+		# byebug
+		 labroom = LabRoom.find_by(id: params[:lab_room_id])
+		 count = params[:array_of_device].size
+
+		 for i in 0...count
+		   code_of_device = params[:array_of_device][i][:code_of_device]
+		   actual_amount = params[:array_of_device][i][:actual_amount]
+		   note = params[:array_of_device][i][:note]
+		   labroom.inventories.create(code_of_device: code_of_device, actual_amount: actual_amount, 
+		   	                           note: note, date_of_inventory: Time.now)
+		   # Time.now.strftime("%d/%m/%Y %H:%M")
+		 end
     end
 end
