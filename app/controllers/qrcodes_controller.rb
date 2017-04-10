e56140dae17b44c9674adbb6c40ce110a9c1685d
linnguyen <nguyenvanlin01041994@gmail.com>
2017-04-10 17:07:38 +0700

    hello heroku

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -4,7 +4,9 @@ class QrcodesController < ApplicationController
   def generate
      if params[:text].present?
         @qr = RQRCode::QRCode.new( params[:text], size: 4)
+     else
+        @qr = RQRCode::QRCode.new( "default", size: 4)
      end
   end
-  
+
 end
